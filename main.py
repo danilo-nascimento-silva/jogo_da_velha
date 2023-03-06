4c7394ebe4393bb75a37fc725c7c49c639b66577
Danilo Nascimento <112949289+danilo-nascimento-silva@users.noreply.github.com>
2023-03-06 17:05:23 -0300

    interface
    Inicio do projeto, ja foi criado a interface junto com a troca de players X e O

---
+++ main.py
@@ -0,0 +1,33 @@
+import PySimpleGUI as sg
+import base
+import time
+
+sg.theme('LightBlue3')
+
+layout = [[sg.Text('Jogo da velha')],
+          [sg.Button('', image_data=base.button_empy, key='1'), sg.Button('', image_data=base.button_empy, key='2'), sg.Button('', image_data=base.button_empy, key='3'), sg.Button('', image_data=base.button_2_p, key='2p')],
+          [sg.Button('', image_data=base.button_empy, key='4'), sg.Button('', image_data=base.button_empy, key='5'), sg.Button('', image_data=base.button_empy, key='6'), sg.Button('', image_data=base.button_maquina, key='machine')],
+          [sg.Button('', image_data=base.button_empy, key='7'), sg.Button('', image_data=base.button_empy, key='8'), sg.Button('', image_data=base.button_empy, key='9'), sg.Button('', image_data=base.button_zerar, key='zerar')]]
+
+window = sg.Window('tesjbdsjfb', layout, grab_anywhere=True)
+player = 1
+
+while True:
+    event, values = window.read()
+    print(event, player)
+    if event and player == 1:
+        print('Jogada play 1')
+        window[event].update(image_data=base.button_x)
+        player = 2
+
+    elif event and player == 2:
+        print('Jogada play 2')
+        window[event].update(image_data=base.button_o)
+        player = 1
+
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
+
+
