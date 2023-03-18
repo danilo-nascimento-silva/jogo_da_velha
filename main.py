eeb46c01c63906187483085a9d140b897fb6598b
Danilo Nascimento <112949289+danilo-nascimento-silva@users.noreply.github.com>
2023-03-18 20:11:11 -0300

    Pronto
    dd

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
 import PySimpleGUI as sg
 import img_base64
 
-possibilidades = [
-    ['1','2','3'],
-    ['4','5','6'],
-    ['7','8','9'],
-    ['1','4','7'],
-    ['2','5','8'],
-    ['3','6','9'],
-    ['1','5','9'],
-    ['3','5','7'],]
+def verifica_ganhador_x():
+    global vencedor 
+    if verifica_jogada[1] == 1 and verifica_jogada[2]  == 1 and verifica_jogada[3] == 1: vencedor = 1
+    elif verifica_jogada[4] == 1 and verifica_jogada[5]  == 1 and verifica_jogada[6] == 1: vencedor = 1
+    elif verifica_jogada[7] == 1 and verifica_jogada[8]  == 1 and verifica_jogada[9] == 1: vencedor = 1
+    elif verifica_jogada[1] == 1 and verifica_jogada[4]  == 1 and verifica_jogada[7] == 1: vencedor = 1
+    elif verifica_jogada[2] == 1 and verifica_jogada[5]  == 1 and verifica_jogada[8] == 1: vencedor = 1
+    elif verifica_jogada[3] == 1 and verifica_jogada[6]  == 1 and verifica_jogada[9] == 1: vencedor = 1
+    elif verifica_jogada[1] == 1 and verifica_jogada[5]  == 1 and verifica_jogada[9] == 1: vencedor = 1
+    elif verifica_jogada[3] == 1 and verifica_jogada[5]  == 1 and verifica_jogada[7] == 1: vencedor = 1
+    return 
+
+def verifica_ganhador_o():
+    global vencedor 
+    if verifica_jogada[1] == 2 and verifica_jogada[2]  == 2 and verifica_jogada[3] == 2: vencedor = 2
+    elif verifica_jogada[4] == 2 and verifica_jogada[5]  == 2 and verifica_jogada[6] == 2: vencedor = 2
+    elif verifica_jogada[7] == 2 and verifica_jogada[8]  == 2 and verifica_jogada[9] == 2: vencedor = 2
+    elif verifica_jogada[1] == 2 and verifica_jogada[4]  == 2 and verifica_jogada[7] == 2: vencedor = 2
+    elif verifica_jogada[2] == 2 and verifica_jogada[5]  == 2 and verifica_jogada[8] == 2: vencedor = 2
+    elif verifica_jogada[3] == 2 and verifica_jogada[6]  == 2 and verifica_jogada[9] == 2: vencedor = 2
+    elif verifica_jogada[1] == 2 and verifica_jogada[5]  == 2 and verifica_jogada[9] == 2: vencedor = 2
+    elif verifica_jogada[3] == 2 and verifica_jogada[5]  == 2 and verifica_jogada[7] == 2: vencedor = 2
+    return 
 
 sg.theme('LightBlue3')
 
 layout = [[sg.Text('Jogo da velha')],
-          [sg.Button('', image_data=img_base64.button_empy, key='1', enable_events=True), sg.Button('', image_data=img_base64.button_empy, key='2'), sg.Button('', image_data=img_base64.button_empy, key='3'), sg.Button('', image_data=img_base64.button_2_p, key='2p')],
+          [sg.Button('', image_data=img_base64.button_empy, key='1'), sg.Button('', image_data=img_base64.button_empy, key='2'), sg.Button('', image_data=img_base64.button_empy, key='3'), sg.Button('', image_data=img_base64.button_2_p, key='2p')],
           [sg.Button('', image_data=img_base64.button_empy, key='4'), sg.Button('', image_data=img_base64.button_empy, key='5'), sg.Button('', image_data=img_base64.button_empy, key='6'), sg.Button('', image_data=img_base64.button_maquina, key='machine')],
           [sg.Button('', image_data=img_base64.button_empy, key='7'), sg.Button('', image_data=img_base64.button_empy, key='8'), sg.Button('', image_data=img_base64.button_empy, key='9'), sg.Button('', image_data=img_base64.button_zerar, key='zerar')]]
 
-verifica_jogada = {1: False, 2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False, 9: False}
+
+verifica_jogada = {1: 0, 2: 0, 3: 0, 
+                   4: 0, 5: 0, 6: 0, 
+                   7: 0, 8: 0, 9: 0}
 
 window = sg.Window('Jogo da Velha', layout, grab_anywhere=True)
 player = 1
 jogadas = 0
-x = []
-o = []
+vencedor = 0
 
 while True:
     event, values = window.read()
@@ -33,32 +49,46 @@ while True:
     elif event == 'machine':
         continue #Vai chamar uma função para jogar contra um Bot 
 
-    elif event == 'zerar' or jogadas == 999:
-        jogadas = 0
+    elif event == 'zerar' or event == 'ok':
         player = 1
-        x = []
-        o = []
+        vencedor = False
         for i in range(1,10):
+            verifica_jogada[i] = 0
             i = str(i)
             window[i].update(image_data=img_base64.button_empy)
 
     elif event and player == 1:
-        window[event].update(image_data=img_base64.button_x)
-        verifica_jogada[int(event)] = event
-        print(verifica_jogada)
-        x.append(event)
-        jogadas += 1
-        player = 2
+        if verifica_jogada[int(event)] == 1 or verifica_jogada[int(event)] == 2:
+            player = 1 
+            continue
+        else:
+            window[event].update(image_data=img_base64.button_x)
+            verifica_jogada[int(event)] = 1
+            jogadas += 1
+            if jogadas >= 3:
+                verifica_ganhador_x()
+            player = 2
 
     elif event and player == 2:
-        window[event].update(image_data=img_base64.button_o)
-        o.append(event)
-        jogadas += 1
-        player = 1
+        if verifica_jogada[int(event)]== 1 or verifica_jogada[int(event)]== 2:
+            player = 2
+            continue
+        else:
+            window[event].update(image_data=img_base64.button_o)
+            verifica_jogada[int(event)] = 2
+            if jogadas >= 3:
+                verifica_ganhador_o()
+            player = 1
+    
+    if vencedor == 1:
+        for i in range(1,10):
+            i = str(i)
+            window[i].update(image_data=img_base64.button_x)    
 
-    # print(f'Casa {event} - Player {player} - N° Jogadas {jogadas}')
-    # print(f'X = {x}')
-    # print(f'O = {o}')
+    if vencedor == 2:
+        for i in range(1,10):
+            i = str(i)
+            window[i].update(image_data=img_base64.button_o)        
 
     if event == sg.WIN_CLOSED:
         break
